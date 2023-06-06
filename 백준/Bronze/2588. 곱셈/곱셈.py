6252cf5416ea1089dcb4ef07f8588dfeb294d870
sg kim <redkafe98@gmail.com>
2023-06-07 03:08:55 +0900

    Update 곱셈.py

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,15 +1,8 @@
 a = int(input())
 b = input()
 
-b_third = int(b[2])
-b_second = int(b[1])
-b_first = int(b[0])
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
 
-b_third_res = a * b_third
-b_second_res = a * (int(b[1]) * 10)
-b_first_res = a * (int(b[0]) * 100)
-
-print(a * b_third)
-print(a * b_second)
-print(a * b_first)
-print(b_first_res + b_second_res + b_third_res)
+print(a*int(b))
