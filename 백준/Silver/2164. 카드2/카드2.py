2a774802d8c4ce7b1e5cdaa1b9cc596e45464cc3
sg kim <redkafe98@gmail.com>
2023-08-29 15:45:09 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 50912 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,54 +1,12 @@
+from collections import deque
+
+
 N = int(input())
+dq = deque(range(1, N + 1))
+
+while len(dq) > 1:
+    dq.popleft()
+    dq.append(dq.popleft())
 
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.length = 0
-
-    def insertAtEnd(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-            self.length += 1
-            return
-        else:
-            current_node = self.tail
-            self.tail = new_node
-            current_node.next = new_node
-            self.length += 1
-
-    def shift(self):
-        if self.head is None:
-            return
-        current = self.head
-        self.head = current.next
-        current.next = None
-        self.length -= 1
-
-    def moveToTheEnd(self):
-        current_head = self.head
-        current_tail = self.tail
-        self.head = self.head.next
-        current_head.next = None
-        self.tail = current_head
-        current_tail.next = current_head
-
-
-list = LinkedList()
-for i in range(1, N + 1):
-    list.insertAtEnd(i)
-
-while list.length > 1:
-    list.shift()
-    list.moveToTheEnd()
-
-print(list.tail.data)
+print(dq.popleft())
