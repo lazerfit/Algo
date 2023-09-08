fe753ebe087650b2f27e20a8adb94d518de6743d
sg kim <redkafe98@gmail.com>
2023-09-08 17:28:56 +0900

    [Silver II] Title: 이항 계수 2, Time: 464 ms, Memory: 46560 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,16 +1,21 @@
 import sys
+
 sys.setrecursionlimit(10**6)
 
 n, r = map(int, input().split())
 board = [[0] * (n + 1) for _ in range(n + 1)]
-for i in range(n + 1):
-    board[i][0] = 1
-    board[i][i] = 1
 
 
 def f(n, r):
-    if board[n][r] == 0:
+    if board[n][r]:
+        return board[n][r]
+
+    if r == 0 or n == r:
+        board[n][r] = 1
+
+    else:
         board[n][r] = f(n - 1, r - 1) + f(n - 1, r)
+
     return board[n][r] % 10007
 
 
