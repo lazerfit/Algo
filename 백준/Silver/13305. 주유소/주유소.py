08c130313a3d8b2914b84e3bd9d8c42769a83258
sg kim <redkafe98@gmail.com>
2023-10-16 21:58:52 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 41844 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -9,14 +9,12 @@ town = list(map(int, input().split()))
 gas = list(map(int, input().split()))
 
 res = 0
-min_gas = gas.index(min(gas[:-2]))
+i = 0
+min_gas = 10001
+while i < len(town):
+    if gas[i] < min_gas:
+        min_gas = gas[i]
+    res += min_gas * town[i]
+    i += 1
 
-if not min_gas:
-    for i in range(len(town)):
-        res += town[i] * gas[min_gas]
-else:
-    for i in range(min_gas):
-        res += town[i] * gas[i]
-    for j in range(min_gas, len(town)):
-        res += town[i] * gas[min_gas]
 print(res)
