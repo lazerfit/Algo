e2d8e7d7c7df77388f79dfee3bc573051fcc9fef
sg kim <redkafe98@gmail.com>
2023-10-31 11:14:40 +0900

    [Silver III] Title: 블로그2, Time: 72 ms, Memory: 44588 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -1,11 +1,8 @@
 n = int(input())
 col = input()
 
-# 'B' 문자열을 모두 '-'로 바꿈 (없어도 될 듯)
-col_b = col.replace('B','-')
-
 # 바꾼 문자열을 기준으로 쪼개서 리스트에 담기
-rr = col_b.split('-')
+rr = col.split('B')
 
 # 위의 과정에서 공백값이 들어가서 공백값을 제거해줌
 rr = list(filter(None, rr))
@@ -13,14 +10,8 @@ rr = list(filter(None, rr))
 # 'B'를 먼저 칠하고 'R'을 칠했을 때 걸리는 작업 횟수
 result_b = len(rr)+1
 
-
-
-
-# 'R' 문자열을 모두 '-'로 바꿈 (없어도 될 듯)
-col_r = col.replace('R','-')
-
 # 바꾼 문자열을 기준으로 쪼개서 리스트에 담기
-bb = col_r.strip().split('-')
+bb = col.strip().split('R')
 
 # 위의 과정에서 공백값이 들어가서 공백값을 제거해줌
 bb = list(filter(None, bb))
@@ -29,4 +20,4 @@ bb = list(filter(None, bb))
 result_r = len(bb) + 1
 
 # 두 가지의 결과중에서 더 적은 수 출력
-print(result_b if result_b < result_r else result_r)
+print(min(result_b,result_r))
