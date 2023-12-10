9f1874911ea82a9225908675ceb80ca110330a1a
sg kim <redkafe98@gmail.com>
2023-12-10 22:48:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for i,j,k in commands:
-        arr_slice=array[i-1:j]
-        arr_slice=sorted(arr_slice)
-        answer.append(arr_slice[k-1])
-    
-    return answer
+    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
