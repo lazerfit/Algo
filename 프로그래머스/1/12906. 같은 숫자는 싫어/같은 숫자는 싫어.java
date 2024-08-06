b2c03e442723e8b82c12fb73424a9bf4366fe75b
sg kim <redkafe98@gmail.com>
2024-08-07 00:21:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.00 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int n = arr.length;
+        
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        
+        for (int i : arr) {
+            if (stack.isEmpty() || !stack.peekLast().equals(i)) {
+                stack.addLast(i);            
+            } 
+        }
+        
+        int[] answer = new int[stack.size()];
+
+        int stackSize = stack.size();
+        
+        for ( int i = 0; i < stackSize; i++) {
+            answer[i] = stack.removeFirst();
+        }
+        
+        return answer;
+    }
+}
