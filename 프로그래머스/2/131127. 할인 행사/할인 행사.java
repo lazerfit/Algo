f15590e68b53d2a27b6a47713c9493327f9e61e8
sg kim <redkafe98@gmail.com>
2024-08-08 22:04:31 +0900

    [level 2] Title: 할인 행사, Time: 78.17 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -8,17 +8,20 @@ class Solution {
             map.put(want[i], number[i]);
         }
         
-        A:for (int i =0;i< discount.length-9;i++ ) {
+        for (int i =0; i < discount.length-9 ; i++ ) {
             HashMap<String, Integer> dis = new HashMap<>();
-            for (int j=i;j<i+10;j++) {
-                dis.put(discount[j],dis.getOrDefault(discount[j],0) + 1);
-            }
-            for (String str:want) {
-                if(map.get(str) != dis.getOrDefault(str,0)) {
-                    continue A;
+            for (int j = i ; j < i + 10 ; j++) {
+                if(map.containsKey(discount[j])) {
+                    dis.put(discount[j], dis.getOrDefault(discount[j],0) +1);
                 }
+                
             }
-            answer++;
+            
+            if (map.equals(dis)) {
+                answer++;
+            }
+            
+            
         }
         
         return answer;
